d5807967ea4bd862ee1435e4d410b3dce6aa45af
Azisten <azistencom@gmail.com>
2022-10-07 09:05:01 +0700

    Fix Absen

--- resources/views/absen/index.blade.php
+++ resources/views/absen/index.blade.php
@@ -18,13 +18,13 @@
                 <span class="inline-block text-3xl font-semibold text-center text-white align-middle selection:bg-violet-600 selection:text-white" id="clock">09:11</span>
             </div>
         </div>
-        <i class="mx-auto mt-20 text-8xl fa-solid fa-angles-down text-violet-600 {{ $absen||$pertemuan->date!=date('Y-m-d')?'opacity-0':'animate-bounce' }}"></i>
+        <i class="mx-auto mt-20 text-8xl fa-solid fa-angles-down text-violet-600 {{ $absen||$pertemuan->date!=date('Y-m-d')||!$pertemuan->status?'opacity-0':'animate-bounce' }}"></i>
         <form action="{{ route('absen.store') }}" method="post" class="w-3/4 mx-auto mt-16 mb-auto">
             @csrf
             <input type="hidden" name="judul" value="{{ $pertemuan->judul }}">
             <input type="hidden" name="pembicara" value="{{ $pertemuan->pembicara }}">
             <input type="hidden" name="poin" value="{{ $pertemuan->poin }}">
-            <button type="submit" {{ $absen||$pertemuan->date!=date('Y-m-d')?'disabled':'' }} class="inline-block w-full px-4 py-2 text-xl font-bold text-center text-white transition-all rounded {{ $absen||$pertemuan->date!=date('Y-m-d')?'bg-gray-400 cursor-not-allowed':'bg-violet-600 hover:no-underline hover:bg-violet-800 ' }}">HADIR</button>
+            <button type="submit" {{ $absen||$pertemuan->date!=date('Y-m-d')||!$pertemuan->status?'disabled':'' }} class="inline-block w-full px-4 py-2 text-xl font-bold text-center text-white transition-all rounded {{ $absen||$pertemuan->date!=date('Y-m-d')||!$pertemuan->status?'bg-gray-400 cursor-not-allowed':'bg-violet-600 hover:no-underline hover:bg-violet-800 ' }}">HADIR</button>
         </form>
     </div>
 </div>
